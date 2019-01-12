77cee43ffedb0b786a16e08303f2b02b5588e2f6
Ondřej Divina <451338@muni.cz>
2019-01-12 21:19:39 +0100

    Nastavení apriori parametrů
    Docela dobré nastavení apriorních parametrů.

--- projekt.m
+++ projekt.m
@@ -14,39 +14,44 @@ addpath('Support\');
 GDP_diff = (GDP(5:end))./GDP(1:end - 4) - 1;
 U_diff = (Uneployment(6:end))- Uneployment(2:end - 4);
 I_diff_a = (Inflation_interannual(6:end)) - Inflation_interannual(2:end - 4);
-R_diff = (Interest_rates(6:end)) - Interest_rates(2:end - 4);
-GDP_diff_lag1 = GDP_diff(1:end - 5);
+% R_diff = (Interest_rates(6:end)) - Interest_rates(2:end - 4);
+
+% U_diff = (Uneployment(6:end))./ Uneployment(2:end - 4) - 1;
+% I_diff_a = (Inflation_interannual(6:end))./ Inflation_interannual(2:end - 4) - 1;
+R_diff = (Interest_rates(6:end))./ Interest_rates(2:end - 4) - 1;
+GDP_diff_lag1 = GDP_diff(1:end-1);
 
 % nakonec jsme radeji pouzili mezictvrtletni rozdily
 % lepsi charakteristiky tykajici se stacionarity
 % vynechali jsme prvni dve pozorovani kvuli tvorbe *_diff promennych (1. pozorovani) a
 % kvuli zpozdene promenne GDP_diff_lag1 (2. pozorovani)
-GDP_diff = (GDP(2:end))./GDP(1:end - 1) - 1;
-U_diff = (Uneployment(3:end)) -Uneployment(2:end - 1);
-I_diff = (Inflation(3:end)) - Inflation(2:end - 1);
-R_diff = (Interest_rates(3:end)) - Interest_rates(2:end - 1);
-GDP_diff_lag1 = GDP_diff(1:end - 1);
-I = Inflation(3:end);
-DATE_short = DATE(3:end);
-U = Uneployment(3:end);
-R = Interest_rates(3:end);
+% GDP_diff = (GDP(2:end))./GDP(1:end - 1) - 1;
+% U_diff = (Uneployment(3:end)) -Uneployment(2:end - 1);
+% I_diff = (Inflation(3:end)) - Inflation(2:end - 1);
+% R_diff = (Interest_rates(3:end)) - Interest_rates(2:end - 1);
+% GDP_diff_lag1 = GDP_diff(1:end - 1);
+I = Inflation(6:end);
+DATE_short = DATE(6:end);
+U = Uneployment(6:end);
+R = Interest_rates(6:end);
 GDP_diff = GDP_diff(2:end);
 
 N_final = length(GDP_diff);
 
 %% StanovenÌ apriornÌch parametr˘
 
-beta_0 = [0; 1; -0.3; -0.3; -1; -1; -2; -2];
+beta_0 = [0; 1; -0.7; -0.2; -1; -1; -2; -2];
 V_0 = diag([0.5^2, 0.2^2, 0.8^2, 0.8^2, 2^2, 2^2, 3^2, 3^2]);
-nu_0 = 3;
-s2_0 = 0.5^2;      %apriorni rozptyl nahodnych slozek - 
+nu_0 = 15;
+s2_0 = var(GDP_diff./4) ;   %tohle je zkouska jestli by to davalo smysl cca
+% s2_0 = 0.05^2;    %apriorni rozptyl nahodnych slozek - 
                   %teda smerodajn· dochylka 10 (n·hodn· chyba m· rozdelenÌ N(0,s^2))
                   %- mohla by byù aj menöia ak by som viac veril svojmu modelu 
 h_0 = 1/s2_0;   %apriorni presnost chyby
 
 %% Definice promÏnnych pro model
 y = GDP_diff;    %vysvetlovana promenna
-X = [ones(N_final,1), GDP_diff_lag1, U, U_diff, I, I_diff, R, R_diff];  %vysvetlujici promenne
+X = [ones(N_final,1), GDP_diff_lag1, U, U_diff, I, I_diff_a, R, R_diff];  %vysvetlujici promenne
 
 X_var_names = cell(1,6);
 X_var_names{1} = 'Konstanta';
@@ -59,8 +64,8 @@ X_var_names{7} = 'Interest rates';
 X_var_names{8} = 'Interest rates difference';
 
 %% Prostor pro vyhozeni nekterych promennych
-ommit_index = [2,3,5,6,8];   %index urcujici, ktere promenne vyhodim
-% ommit_index = [1,2,6];
+ommit_index = [2,6,8];   %index urcujici, ktere promenne vyhodim
+% ommit_index = [2,3,5,6,8];
 X_var_names(ommit_index) = [];
 %kdyz chci pouzit model s vynechanim nekterych promennych
    X(:, ommit_index) = [];
@@ -82,8 +87,10 @@ for (i=1:length(test_vars))
         test_vars(i),test_values(i),SD_ratio(i));
 end
 
+fprintf('CD statistika byla: %7.4f\n',cng.CD);
+
 for i = 1:size(beta,1)
-   figure
+   subplot(3,ceil(length(beta_0)/3),i);
    hist(beta(i,:),100);
    title(X_var_names(i));
 end

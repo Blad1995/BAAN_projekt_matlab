985c8ae956be3662e7db8686e87cb8dfce4ff7ca
Ondřej Divina <451338@muni.cz>
2019-01-12 22:36:55 +0100

    Vymazání GPD_diff_lag a oprava h vypisu

--- projekt.m
+++ projekt.m
@@ -12,14 +12,13 @@ addpath('Support\');
 % mezirocni procentualni rust realneho GDP (uz zkraceneho o 
 % prvni obdobi - kvuli zpozdene promenne)
 GDP_diff = (GDP(5:end))./GDP(1:end - 4) - 1;
-U_diff = (Uneployment(6:end))- Uneployment(2:end - 4);
-I_diff_a = (Inflation_interannual(6:end)) - Inflation_interannual(2:end - 4);
+U_diff = (Uneployment(5:end))- Uneployment(1:end - 4);
+I_diff_a = (Inflation_interannual(5:end)) - Inflation_interannual(1:end - 4);
 % R_diff = (Interest_rates(6:end)) - Interest_rates(2:end - 4);
 
 % U_diff = (Uneployment(6:end))./ Uneployment(2:end - 4) - 1;
 % I_diff_a = (Inflation_interannual(6:end))./ Inflation_interannual(2:end - 4) - 1;
-R_diff = (Interest_rates(6:end))./ Interest_rates(2:end - 4) - 1;
-GDP_diff_lag1 = GDP_diff(1:end-1);
+R_diff = (Interest_rates(5:end))./ Interest_rates(1:end - 4) - 1;
 
 % nakonec jsme radeji pouzili mezictvrtletni rozdily
 % lepsi charakteristiky tykajici se stacionarity
@@ -30,18 +29,17 @@ GDP_diff_lag1 = GDP_diff(1:end-1);
 % I_diff = (Inflation(3:end)) - Inflation(2:end - 1);
 % R_diff = (Interest_rates(3:end)) - Interest_rates(2:end - 1);
 % GDP_diff_lag1 = GDP_diff(1:end - 1);
-I = Inflation(6:end);
-DATE_short = DATE(6:end);
-U = Uneployment(6:end);
-R = Interest_rates(6:end);
-GDP_diff = GDP_diff(2:end);
+I = Inflation(5:end);
+DATE_short = DATE(5:end);
+U = Uneployment(5:end);
+R = Interest_rates(5:end);
 
 N_final = length(GDP_diff);
 
 %% Stanovení apriorních parametrù
 
-beta_0 = [0; 1; -0.7; -0.2; -1; -1; -2; -2];
-V_0 = diag([0.5^2, 0.2^2, 0.8^2, 0.8^2, 2^2, 2^2, 3^2, 3^2]);
+beta_0 = [0;-0.7; -0.2; -1; -1; -2; -2];
+V_0 = diag([0.5^2, 0.8^2, 0.8^2, 2^2, 2^2, 3^2, 3^2]);
 nu_0 = 15;
 s2_0 = var(GDP_diff./4) ;   %tohle je zkouska jestli by to davalo smysl cca
 % s2_0 = 0.05^2;    %apriorni rozptyl nahodnych slozek - 
@@ -51,21 +49,19 @@ h_0 = 1/s2_0;   %apriorni presnost chyby
 
 %% Definice promìnnych pro model
 y = GDP_diff;    %vysvetlovana promenna
-X = [ones(N_final,1), GDP_diff_lag1, U, U_diff, I, I_diff_a, R, R_diff];  %vysvetlujici promenne
+X = [ones(N_final,1), U, U_diff, I, I_diff_a, R, R_diff];  %vysvetlujici promenne
 
 X_var_names = cell(1,6);
 X_var_names{1} = 'Konstanta';
-X_var_names{2} = 'Y_{t-1}';
-X_var_names{3} = 'Unemployment';
-X_var_names{4} = 'Unemployment difference';
-X_var_names{5} = 'Inflation'; 
-X_var_names{6} = 'Inflation difference'; 
-X_var_names{7} = 'Interest rates';
-X_var_names{8} = 'Interest rates difference';
+X_var_names{2} = 'Unemployment';
+X_var_names{3} = 'Unemployment difference';
+X_var_names{4} = 'Inflation'; 
+X_var_names{5} = 'Inflation difference'; 
+X_var_names{6} = 'Interest rates';
+X_var_names{7} = 'Interest rates difference';
 
 %% Prostor pro vyhozeni nekterych promennych
-ommit_index = [2,6,8];   %index urcujici, ktere promenne vyhodim
-% ommit_index = [2,3,5,6,8];
+ommit_index = [4,5,7];   %index urcujici, ktere promenne vyhodim
 X_var_names(ommit_index) = [];
 %kdyz chci pouzit model s vynechanim nekterych promennych
    X(:, ommit_index) = [];
@@ -102,8 +98,8 @@ for i=1:length(beta_0)
     fprintf('beta_%d\t\t%6.4f\t\t\t%6.4f\t\t\t%6.4f\t\t\t%6.4f\t\t\t%6.4f\n',...
         i,beta_means(i),beta_sd(i),HPDI_beta(i,:),cng.CD(i));
 end
-fprintf('h\t\t\t%6.4f\t\t%6.4f\t\t\t\t\t\t\t\t\t\t%6.4f\t\t\t%6.4f\n',...
-        mean(h),sqrt(var(h)),cng.CD(i));
+fprintf('h\t\t\t%6.4f\t\t%6.4f\t\t\t\t\t\t\t\t\t\t%6.4f\n',...
+        mean(h),sqrt(var(h)),cng.CD(length(beta_0)+1));
 
 for i = 1:size(beta,1)
    subplot(3,ceil(length(beta_0)/3),i);
